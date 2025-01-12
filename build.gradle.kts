e0d654f780979b4c800883b7c6c2e0eab301d3c2
Gabriele Cabrini <gabrielecabriniii@gmail.com>
2025-01-12 23:28:27 +0100

    chore: add fatJar gradle task

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,14 @@
 plugins {
-    kotlin("jvm") version "2.0.21"
+    application
+    kotlin("jvm") version "2.1.0"
 }
 
 group = "it.gabrielecabrini"
 version = "0.1.0"
+
+application {
+    mainClass = "it.gabrielecabrini.kosh.KoshKt"
+}
 
 repositories {
     mavenCentral()
@@ -20,4 +25,27 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(21)
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21)) // Set Java version explicitly
+    }
+}
+
+tasks {
+    val fatJar = register<Jar>("fatJar") {
+        dependsOn.addAll(listOf("compileJava", "compileKotlin", "processResources"))
+        archiveClassifier.set("standalone") // Naming the jar
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+        manifest { attributes(mapOf("Main-Class" to application.mainClass)) }
+        val sourcesMain = sourceSets.main.get()
+        val contents = configurations.runtimeClasspath.get()
+            .map { if (it.isDirectory) it else zipTree(it) } +
+                sourcesMain.output
+        from(contents)
+    }
+    build {
+        dependsOn(fatJar) // Trigger fat jar creation during build
+    }
 }
