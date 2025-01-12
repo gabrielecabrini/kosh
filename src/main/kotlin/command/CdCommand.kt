6364b6f4c5f9513dfabe19df38cb18681e811163
Gabriele Cabrini <gabrielecabriniii@gmail.com>
2025-01-12 21:29:44 +0100

    fix(core): fix cd absolute paths

--- src/main/kotlin/command/CdCommand.kt
+++ src/main/kotlin/command/CdCommand.kt
@@ -9,11 +9,18 @@ class CdCommand : BuiltinCommand() {
             changeWorkingDirectory(System.getProperty("user.home"))
             return
         }
-        val target = File(currentDirectory, args[0]).canonicalFile
+
+        val targetPath = args[0]
+        val target = if (targetPath.startsWith("/")) {
+            File(targetPath).canonicalFile
+        } else {
+            File(currentDirectory, targetPath).canonicalFile
+        }
+
         if (target.exists() && target.isDirectory) {
             changeWorkingDirectory(target.path)
         } else {
-            throw RuntimeException("cd: ${args[0]}: No such file or directory")
+            throw RuntimeException("cd: ${target.path}: No such file or directory")
         }
     }
 }
